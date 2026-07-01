24c835b5a665618f702160e69a8f6d1a00944496
Leonorenexus <leonorenexus@gmail.com>
2026-07-02 04:25:54 +0700

    Create Color.kt

---
+++ app/src/main/java/com/genetic/system/v3/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.genetic.system.v3.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val RedDark = Color(0xFF8B0000)
+val GreenDark = Color(0xFF006400)
+val BlueDark = Color(0xFF00008B)
+val PurpleDark = Color(0xFF4A148C)
+val CyanDark = Color(0xFF008B8B)
